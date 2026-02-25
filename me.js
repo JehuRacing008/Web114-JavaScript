a4565e3290e8ee10e8804cd86e3fe5df00a2dca7
JehuRacing008 <nmonning@stumail.jccc.edu>
2026-02-25 11:05:38 -0600

    Add files via upload

---
+++ me.js
@@ -0,0 +1,13 @@
+// Noah Monnington All about me 2/25/26
+
+let myName = "Noah";
+let favColor = "Forest Green";
+let favHobby = "Playing Volleyball";
+let favFood = "Anything from Panda Express";
+
+//display information
+console.log("My Name is:", myName);
+console.log("My Color is:", favColor);
+console.log("My Hobby is:", favHobby);
+console.log("My Food is:", favFood);
+console.log("My Favorite animal is:", myName);
